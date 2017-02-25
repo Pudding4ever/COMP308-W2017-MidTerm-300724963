94095c6a02ebc001d8c8313aa8d85049e4fc60e8
Pudding4ever <dkerrcresswell@gmail.com>
2017-02-25 09:32:55 -0500

    more changes cause I did a stupid

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://thomas:123456@ds161059.mlab.com:61059/books308"
+  "URI": "mongodb://test:swordfish@ds145299.mlab.com:45299/testdbformidterm"
   //Remote MongoDB deployment -> let URI = "mongodb://thomas:123456@ds161059.mlab.com:61059/books308";
 };
