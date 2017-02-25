06e3b3b504258c3257eebf06bbac68a902c5a50d
Pudding4ever <dkerrcresswell@gmail.com>
2017-02-25 15:37:16 -0500

    test db stuff

--- server/routes/books.js
+++ server/routes/books.js
@@ -35,7 +35,7 @@ res.render('books/details', {
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
-
+db.books.insert({"Title":"test", "Description":"test", "Price": 6, "Author":"Dave", "Genre":"TEST"});
     /*****************
      * ADD CODE HERE *
      *****************/
